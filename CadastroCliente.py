3ba3ae81498ad9d23b920c133b0f780283f1922d
Adrian Lemos <adrianaugustodelemo@gmail.com>
2026-09-01 20:37:13 -0300

    Cadastro de Usuário

---
+++ CadastroCliente.py
@@ -0,0 +1,34 @@
+    
+
+clientes = []
+produtos = []
+vendas = []
+
+historico = []
+
+
+def cadastrar_cliente():
+    nome = input ("Digite o nome do cliente: ")
+    cpf = input ("Digite o CPF do cliente: ")
+
+    cliente ={
+        "nome": nome,
+        "cpf": cpf
+    }
+
+    clientes.append(cliente)
+
+    print("Cliente cadastrado com sucesso!")
+
+
+def listar_cliente():
+    if len(clientes) == 0:
+        print("Nenhum cliente cadastrado.")
+        return
+
+    print("\n --- CLIENTES CADASTRADOS---")
+
+    for cliente in clientes:
+        print("Nome:", cliente["nome"])
+        print("CPF", cliente["cpf"] )
+        print(------------------------------------) 
